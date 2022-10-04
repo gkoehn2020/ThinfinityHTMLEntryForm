e5336be689528f8656d812675b12677575d74d5c
gkoehn2020 <59707158+gkoehn2020@users.noreply.github.com>
2022-10-03 18:10:11 -0700

    Update HTMLEntryForm.js

--- html/HTMLEntryForm.js
+++ html/HTMLEntryForm.js
@@ -1,6 +1,6 @@
 
 const componentOneStyle = `@charset "utf-8";
-.loading-overlay {
+.orOverlay {
     background: rgba( 26,26,26,0.7 );
     /* position: fixed; */
     width: 100%;
@@ -9,7 +9,7 @@ const componentOneStyle = `@charset "utf-8";
     top: 0;
 }
 
-.loading-overlay-img-container {
+.orOverlayContainer {
     position: absolute;
     z-index: 7;
     top: 50%;
@@ -17,28 +17,44 @@ const componentOneStyle = `@charset "utf-8";
     transform: translate( -50%, -50% );
 }
 
-.loading-overlay-img {
-    width: 160px;
-    height: 24px;
-    border-radius: 5px;
-}
-
-.orion-working-hidden {
+.orModalHidden {
     display: none;
 }
 
-#orion-working-msg {
-  text-align: center;
-  color: white;
+.ormodal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  max-width: 760px;
+  min-width: 759px;
 }
+
+.orNarrative {
+  resize: none;
+}
+
+.orBtnCancel {
+  float: right;
+  margin-right: 4px; 
+  width: 100px;
+}
+
+.orBtnsave {
+  float: right;
+  margin-right: 10px;
+  width: 100px;
+}
+
 `;
 
 const addCSS = codeToAdd => document.head.appendChild(document.createElement("style")).innerHTML = codeToAdd;
 addCSS(componentOneStyle);
 
 xtag.register('x-entryform1', {
-  content: `<div class="loading-overlay orion-working-hidden"></div>
-            <div class="loading-overlay-img-container orion-working-hidden">
+  content: `<div class="orOverlay orModalHidden"></div>
+            <div class="orOverlayContainer orModalHidden">
               <!-- The Modal -->
               <div id="orModal" class="ormodal">
                 <!-- Modal content -->
@@ -46,12 +62,12 @@ xtag.register('x-entryform1', {
                    
                   <form id="form">
                 
-                    <p>Orion Dictation Entry</p>
+                    <p>Dictation Entry</p>
                     <textarea id="orNarrative" name="orNarrative" rows="20" cols="100" class="orNarrative"></textarea> 
                     <div>
                       <i class="pointer fa fa-2xs fa-microphone-slash" id="mic"></i>
-                      <button id="orBtnCancel" type="button" class="btncancel">Cancel</button>
-                      <button class="btnsave">Save</button>
+                      <button id="orBtnCancel" type="button" class="orBtnCancel">Cancel</button>
+                      <button class="orBtnsave">Save</button>
                     </div>
                   
                   </form>
@@ -72,18 +88,18 @@ xtag.register('x-entryform1', {
       });
       
       jsro.on(_controlID, "ShowThisModal", function () {
-        const div =  document.querySelector('.loading-overlay');
-        div.classList.remove('orion-working-hidden');
-        const div2 =  document.querySelector('.loading-overlay-img-container');
-        div2.classList.remove('orion-working-hidden');     
+        const div =  document.querySelector('.orOverlay');
+        div.classList.remove('orModalHidden');
+        const div2 =  document.querySelector('.orOverlayContainer');
+        div2.classList.remove('orModalHidden');     
         console.trace('start');
       });
 
       document.getElementById("orBtnCancel").onclick = function() {
-        const div =  document.querySelector('.loading-overlay');
-        div.classList.add('orion-working-hidden');
-        const div2 =  document.querySelector('.loading-overlay-img-container');
-        div2.classList.add('orion-working-hidden');
+        const div =  document.querySelector('.orOverlay');
+        div.classList.add('orModalHidden');
+        const div2 =  document.querySelector('.orOverlayContainer');
+        div2.classList.add('orModalHidden');
         ro.orCancel();
       };   
       
